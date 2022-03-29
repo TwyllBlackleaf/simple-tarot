2d260b4f1dd88de45ebd8a87b569fa10aad5c4f6
Twyll <twyllblackleaf@gmail.com>
2022-03-28 22:33:38 -0400

    sketch out structure of javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+const cardsForm = document.getElementById("cards-form");
+
+const displayCards = function(event) {
+    event.preventdefault();
+
+    
+}
+
+const rollCards = function(cardNumber, inverted) {
+    
+}
+
+cardsForm.onsubmit = displayCards;
